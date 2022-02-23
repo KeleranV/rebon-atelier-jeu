c432ff495d0cf8c45104748322c3a2b7c1710568
LancelotDeperceval <desharnaiskevin@gmail.com>
2022-02-23 15:13:12 -0500

    TownMenu ver.1

---
+++ TownMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_1
+{
+    public class TownMenu
+    {
+        public void Town()
+        {
+            Console.WriteLine();
+            Console.WriteLine("                                                      **Town**\n" +
+                                    "\n" +
+
+                              "                                                     1-Marchant\n" +
+                              "                                                     2-Forgeron\n" +
+                              "                                                     3-Alchemiste\n" +
+                              "                                                     4-Auberge\n" +
+                              "                                                     5-Jean-Guy\n" +
+                              "                                                     6-Arène\n");
+        }
+    }
+}
