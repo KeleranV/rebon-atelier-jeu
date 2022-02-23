cab0d6e72961f6b4d3f47eb638ad6db61f50a149
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-02-23 14:42:35 -0500

    added consumableItem

--- InventorySystem/Item.cs
+++ InventorySystem/Item.cs
@@ -2,20 +2,20 @@
 {
     public abstract class Item
     {
-        protected string m_Name = "";
-        protected int m_Price = 0;
-        protected EQuality m_Quality = EQuality.Poor;
+        protected string p_Name = "";
+        protected int p_Price = 0;
+        protected EQuality p_Quality = EQuality.Poor;
 
-        public int Price { get => m_Price; }
-        public string Name { get => m_Name; }
-        public EQuality Quality { get => m_Quality; }
+        public int Price { get => p_Price; }
+        public string Name { get => p_Name; }
+        public EQuality Quality { get => p_Quality; }
 
 
         public Item(int a_Price, string a_Name, EQuality a_Quality)
         {
-            m_Price = a_Price;
-            m_Name = a_Name;
-            m_Quality = a_Quality;
+            p_Price = a_Price;
+            p_Name = a_Name;
+            p_Quality = a_Quality;
         }
 
         public abstract void DrawItem();
