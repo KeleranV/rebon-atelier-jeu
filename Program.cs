51b49dcff9300954a2ce1c26b45e69cf4299591c
Alex Demars <a.demars@insa-cvl.fr>
2022-02-25 09:48:53 -0500

    REstructuration du cobtrolleur (Program.cs)

--- Program.cs
+++ Program.cs
@@ -2,22 +2,21 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
+
+        private static Player CreationPersonnage()
         {
             Console.WriteLine("Entrez le nom de votre personnage :");
             string name = Console.ReadLine();
             Console.WriteLine("Dites nous votre genre");
             string genre = Console.ReadLine();
-            Player player = new Player(name, 100, 10, genre);
 
-           
+            return new Player(name, 100, 10, genre);
+        }
 
-            Monster monster = Monster.GenerateMonster(Monster.RandomizeMonsterName(), Monster.RandomizeMonsterMaxHealth(), Monster.RandomizeMonsterDamage(), player, Monster.MonsterRewardCalculation(Monster.RandomizeMonsterMaxHealth(), Monster.RandomizeMonsterDamage()));
-            //monster.Attack();
-
-            int vieARegenerer = player.MaxHealth / 4;
-            Duel duel1 = new Duel(player, monster, vieARegenerer);
-
+        private static void GestionCombats(Player player, Monster firstMonster, int vieARegenerer)
+        {
+            Duel duel1 = new Duel(player, firstMonster, vieARegenerer);
+            Monster monster = firstMonster;
             while (player.Health > 0)
             {
                 duel1.LancerCombat();
@@ -27,6 +26,27 @@
                     duel1 = new Duel(player, monster, vieARegenerer);
                 }
             }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Player player = CreationPersonnage();
+
+            Monster monster = Monster.GenerateMonster(
+                Monster.RandomizeMonsterName(),
+                Monster.RandomizeMonsterMaxHealth(),
+                Monster.RandomizeMonsterDamage(),
+                player,
+                Monster.MonsterRewardCalculation(Monster.RandomizeMonsterMaxHealth(), Monster.RandomizeMonsterDamage())
+                );
+
+            int vieARegenerer = player.MaxHealth / 4;
+
+
+            GestionCombats(player, monster, vieARegenerer);
+
+
             Console.WriteLine("Game Over");
         }
     }
